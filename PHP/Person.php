34c714e04146aa47362977b7c5ff01bd8b177e5a
Jacob Stevens <cobymstevens44@gmail.com>
2017-10-10 00:25:28 -0400

    Fixed person class by adding getter and setter for age variable

--- PHP/Person.php
+++ PHP/Person.php
@@ -1,9 +1,27 @@
 <?php
 
 class person {
+
+  public $name;
+
   private $age;
-  public function(){
-    
+
+  public function __construct($name)
+  {
+      $this->name=$name;
+  }
+  public function getAge()
+  {
+    return $this->age;
+  }
+  public function setAge($age){
+    if ($age < 21) {
+      throw new Exception("Not Allowed to Drink");
+    }
+    $this->age=$age;
   }
 }
+$Jake = new person("Jacob");
+$Jake->setAge(21);
+var_dump($Jake->getAge())
 ?>
