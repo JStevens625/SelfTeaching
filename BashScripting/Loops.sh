cc1a437c81687a529e0af93f0665b7914b358ebd
Jacob Stevens <cobymstevens44@gmail.com>
2019-01-15 11:50:40 -0500

    Finished Learning Loops

--- BashScripting/Loops.sh
+++ BashScripting/Loops.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
 #While Loop
-while [1 = 1]
+echo While Loop
+i=0
+while [ $i -le 10 ]
 do
-    echo Hi
+    ((i+=1))
+    echo $i
+done
+
+#For Loop
+echo For Loop
+j=0
+for j in {0..10..2}
+do
+    echo $j
+done
+
+#Until Loop
+echo Until Loop
+k=0
+until [ $k -ge 10 ]
+do
+    ((k+=3))
+    echo $k
 done
