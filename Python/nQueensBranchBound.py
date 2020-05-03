c72cccf1fbc01239b28657a4ee3233f8a14b8ebd
Jacob <cobymstevens44@gmail.com>
2020-05-02 23:36:24 -0400

    Tested nQueens Problem in a program

---
+++ Python/nQueensBranchBound.py
@@ -0,0 +1,14 @@
+
+def createBoard(numQueen):
+    arr = []
+    for i in range(numQueen):
+        arr.append([])
+    # for i in range(numQueen):
+    #     arr.append('-')
+    return arr
+
+def printBoard(board):
+    length = 
+
+if __name__ == '__main__':
+    board = createBoard(5)
