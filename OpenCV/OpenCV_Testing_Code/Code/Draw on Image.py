d21fae573f15824dda4338021e15c4905fa8c845
Jacob <cobymstevens44@gmail.com>
2021-05-09 09:53:18 -0400

    Updated Opencv file

--- OpenCV/OpenCV_Testing_Code/Code/Draw on Image.py
+++ OpenCV/OpenCV_Testing_Code/Code/Draw on Image.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('SampleData/aero1.jpg',1)
+img = cv2.imread('../SampleData/aero1.jpg',1)
 line = cv2.line(img,(0,0),(255,255),(0,0,255),10)
 arrow1 = cv2.arrowedLine(img,(0,255),(255,255),(255,0,0),10)
 rec = cv2.rectangle(img,(50,50),(100,300),(0,255,0),-1)
