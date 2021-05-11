3b87f3978ce3b410270b72e810effa0f966c964b
Jacob <cobymstevens44@gmail.com>
2021-05-11 09:45:34 -0400

    Made initial Person class all public currently

--- PHP/Person.php
+++ PHP/Person.php
@@ -8,9 +8,24 @@ class Person {
   {
     $this->name = $name;
   }
+
+  public function getAge()
+  {
+    return $this->age;
+  }
+
+  public function setAge($age)
+  {
+    if ($age < 18) {
+      throw new Exception("Not old enough");
+    }
+
+    $this->age = $age;
+  }
+
 }
-
 $john = new Person("John Doe");
-$john->age = 30;
+$john->setAge(18);
 
+var_dump($john->getAge())
 ?>
