3e42bd0334defcfa801336cf17e6e99ada993b41
Jacob <cobymstevens44@gmail.com>
2021-05-12 22:13:04 -0400

    Starting messages in OOP

---
+++ PHP/Messages.php
@@ -0,0 +1,22 @@
+<?php
+
+class Person {
+  protected $name;
+  public function __construct($name)
+  {
+    $this->name = $name;
+  }
+}
+
+class Business {
+  public function hire(Person $person)
+  {
+    // code...
+  }
+}
+
+class Staff {
+
+}
+
+?>
