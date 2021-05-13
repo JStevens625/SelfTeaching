27645d945ac2804d48f894d3abd3c1cb71019559
Jacob <cobymstevens44@gmail.com>
2021-05-12 22:03:24 -0400

    Understood abstract usage for requirement of a method/function without being able to use an abstract class

--- PHP/Shape.php
+++ PHP/Shape.php
@@ -1,6 +1,21 @@
 <?php
 
-class Shape{
+abstract class Shape{
+  protected $color;
+  public function __construct($color = 'red') //Given a default color of red.
+  {                                           //If default was not given then
+    $this->color = $color;                    //shape could not be used till color was defined.
+  }
+
+  public function getColor()
+  {
+      return $this->color;
+  }
+
+  abstract protected function getArea();
+}
+
+class Square extends Shape {
   protected $length = 4;
 
   public function getArea()
@@ -9,19 +24,26 @@ class Shape{
   }
 }
 
-class Square extends Shape {
-
-}
-
 class Triangle extends Shape {
   protected $base = 4;
   protected $height = 7;
 
   public function getArea(){
-    return .5 * $this->base * $this->height;
+    return .5 * $this->base * $this->height; //Overrides the parent method
   }
 }
 
-echo (new Triangle)->getArea();
-echo (new Square)->getArea();
+class Circle extends Shape {
+  protected $radius = 5;
+  public function getArea()
+  {
+    return pi * pow($this->radius,2);
+  }
+}
+
+//echo (new Triangle)->getArea();
+//echo (new Square)->getColor(); //default red
+//echo (new Circle('green'))->getColor(); //green
+//new Shape; //Cannot be done because it is abstract. But will have requirements
+//new Circle;
 ?>
