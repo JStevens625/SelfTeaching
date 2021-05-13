11ae8ac29f7ac8ab156eb6809459e1052b21db51
Jacob <cobymstevens44@gmail.com>
2021-05-12 22:52:17 -0400

    learned importing and understanding of pulling in classes

--- PHP/Messages.php
+++ PHP/Messages.php
@@ -1,53 +1,9 @@
 <?php
 
-class Person {
-  protected $name;
-  public function __construct($name)
-  {
-    $this->name = $name;
-  }
-}
-
-class Business {
-
-  protected $staff;
-
-  public function __construct(Staff $staff)
-  {
-    $this->staff = $staff;
-  }
-
-  public function hire(Person $person)
-  {
-    $this->staff->add($person);
-  }
-
-  public function getStaffMembers()
-  {
-    return $this->staff->getMembers();
-  }
-}
-
-class Staff {
-
-  protected $members = [];
-
-  public function __construct($members = [])
-  {
-    $this->members = $members;
-  }
-
-  public function add(Person $person)
-  {
-    $this->members[] = $person;
-  }
-
-  public function getMembers()
-  {
-    return $this->members;
-  }
-}
-
+require 'autoloading/Person.php';
+require 'autoloading/Business.php';
+require 'autoloading/Staff.php';
+//Instead of requireing files we can autoload using composer for now we will manually import them.
 $person1 = new Person('Joe Smith');
 
 $person2 = new Person('Jane Doe');
@@ -59,4 +15,5 @@ $business = new Business($staff);
 $business->hire($person2);
 
 var_dump($business->getStaffMembers());
+
 ?>
