60e70e6dbcfe35a4351197449b460fd0415e429f
Jacob <cobymstevens44@gmail.com>
2021-05-12 22:39:41 -0400

    Understood the concept of how classes send messages to each other

--- PHP/Messages.php
+++ PHP/Messages.php
@@ -9,14 +9,54 @@ class Person {
 }
 
 class Business {
+
+  protected $staff;
+
+  public function __construct(Staff $staff)
+  {
+    $this->staff = $staff;
+  }
+
   public function hire(Person $person)
   {
-    // code...
+    $this->staff->add($person);
+  }
+
+  public function getStaffMembers()
+  {
+    return $this->staff->getMembers();
   }
 }
 
 class Staff {
 
+  protected $members = [];
+
+  public function __construct($members = [])
+  {
+    $this->members = $members;
+  }
+
+  public function add(Person $person)
+  {
+    $this->members[] = $person;
+  }
+
+  public function getMembers()
+  {
+    return $this->members;
+  }
 }
 
+$person1 = new Person('Joe Smith');
+
+$person2 = new Person('Jane Doe');
+
+$staff = new Staff([$person1]);
+
+$business = new Business($staff);
+
+$business->hire($person2);
+
+var_dump($business->getStaffMembers());
 ?>
