6ab835310c1396ceb8d4a7bf52b81e126ec9b3d7
Jacob <cobymstevens44@gmail.com>
2021-08-21 11:15:29 -0400

    Made Parent Class of Pet and had Dog class inherit Pet

--- Python/OOP/Pet.py
+++ Python/OOP/Pet.py
@@ -1,17 +1,19 @@
-class Dog(object):
+class Pet:
     def __init__(self, name, age):
         self.name = name
         self.age = age
     
-    def speak(self):
-        print("Bark")
-    
     def show(self):
         print(f"My name is {self.name} and I am {self.age} years old!")
-    
+
     def set_age(self, age):
         self.age = age
 
+class Dog(Pet):
+    
+    def speak(self):
+        print("Bark")
+        
 tim = Dog("Tim",4)
 bill = Dog("Bill",9)
 
