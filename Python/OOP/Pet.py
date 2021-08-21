3dfe45a1d7ae22d0b679b2642120f581dddc6310
Jacob <cobymstevens44@gmail.com>
2021-08-21 11:34:15 -0400

    Created cat class that inherits pet class

--- Python/OOP/Pet.py
+++ Python/OOP/Pet.py
@@ -8,17 +8,30 @@ class Pet:
 
     def set_age(self, age):
         self.age = age
+    
+    def speak(self):
+        print("What am I?")
 
 class Dog(Pet):
     
     def speak(self):
-        print("Bark")
-        
-tim = Dog("Tim",4)
+        print("Bork Bork!")
+
+class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+    
+    def speak(self):
+        print("Meow Meow!")
+    
+    
+
+tim = Cat("Tim",4, "Calico")
 bill = Dog("Bill",9)
 
 tim.set_age(15)
 
 tim.show()
 bill.show()
-
+tim.speak()
