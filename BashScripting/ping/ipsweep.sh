c7e04e1b36992ff219fee61b9277a7896eb7f4ad
Jacob S <cobymstevens44@gmail.com>
2023-05-23 21:54:02 -0400

    Replacement of backticks with newer standard of performing the commands

--- BashScripting/ping/ipsweep.sh
+++ BashScripting/ping/ipsweep.sh
@@ -6,7 +6,7 @@ echo "You forgot an IP address!"
 echo "Syntax: ./ipsweep.sh 192.168.1"
 
 else
-for ip in `seq 1 254`;
+for ip in $(seq 1 254);
 do
 ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 done
