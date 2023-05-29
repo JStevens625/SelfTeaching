ecb0ec0b40c27d1a07a89a784c5636dfabafce76
Jacob S <cobymstevens44@gmail.com>
2023-05-28 23:57:52 -0400

    Added group argument for verbose and quiet based on preferred method of printing

--- Python/ArgParse/Init.py
+++ Python/ArgParse/Init.py
@@ -8,6 +8,10 @@ parser = argparse.ArgumentParser(description="Calculate the volume of a cylinder
 parser.add_argument('-r','--radius',type=int, metavar='', required=True, help="Define the radius of cylinder")
 parser.add_argument('-H','--height',type=int, metavar='', required=True, help="Define the height of cylinder")
 
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-q', '--quiet', action='store_true', help='Print Quiet')
+group.add_argument('-v', '--verbose', action='store_true', help='Print Verbose')
+
 args = parser.parse_args()
 
 def cylinder_volume(radius,height):
@@ -15,4 +19,9 @@ def cylinder_volume(radius,height):
     return volume
 
 if __name__ == '__main__':
-    print (cylinder_volume(args.radius,args.height))
+    volume = cylinder_volume(args.radius, args.height)
+    if args.quiet:
+        print(volume)
+    elif args.verbose:
+        print(f"Volume of a Cylinder with radius {args.radius} and height {args.height} is {volume}")
+    else: print(f"Volume of Cylinder is {volume}")
