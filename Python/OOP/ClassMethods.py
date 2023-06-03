a56c8ce52a7c5c7c3a733d3fad9eb38beb90d4f0
Jacob S <cobymstevens44@gmail.com>
2023-06-03 17:03:44 -0400

    Learned Python Class Methods and the use of the staticmethod

---
+++ Python/OOP/ClassMethods.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Math():
+    
+    @staticmethod
+    def add5(x):
+        return x + 5
+
+    @staticmethod
+    def add5(x):
+        return x + 10
+
+    @staticmethod
+    def pr():
+        print(run)
+
+print(Math.add5(5))
