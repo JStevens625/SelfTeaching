12e329c5a69a35a7cc130c21e0ec6844cb99459d
Jacob S <cobymstevens44@gmail.com>
2023-06-03 16:58:45 -0400

    Learned about classmethods

--- Python/OOP/ClassAttributes.py
+++ Python/OOP/ClassAttributes.py
@@ -5,10 +5,17 @@ class Person:
 
     def __init__(self, name):
         self.name = name
-        Person.number_of_people += 1
+        Person.add_person()
 
-print(Person.number_of_people)
+    @classmethod
+    def number_of_people_(cls):
+        return cls.number_of_people
+
+    @classmethod
+    def add_person(cls):
+        cls.number_of_people +=1
+
 p1 = Person("Tim")
-print(Person.number_of_people)
 p2 = Person("Jill")
-print(Person.number_of_people)
+print(Person.number_of_people_())
+
